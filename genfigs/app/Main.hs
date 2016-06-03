83a35875e44ed7e37b6121aeb48a40d55febda05
David Turner <dave.c.turner@gmail.com>
2016-06-03 15:20:21 +0000

    Figs and stuff

--- genfigs/app/Main.hs
+++ genfigs/app/Main.hs
@@ -4,6 +4,8 @@ import Lib
 import System.IO
 import Control.Monad.Writer
 import Text.Printf
+import System.Process
+import Data.List
 
 xLeader, x1, x2 :: Int
 x1      = 1000
@@ -19,8 +21,8 @@ tellTimeline n x label = do
   tellCoords x y x (y + timelineLen)
   tell "2 1 0 1 0 7 50 -1 -1 0.000 0 0 -1 0 0 2\n"
   tellCoords (x - halfHeaderWidth) y (x + halfHeaderWidth) y
-  tell $ printf "4 1 0 50 -1 0 12 0.0000 2 135 540 %d %d %s\\001\n"
-    x (y-90) label
+  tell $ printf "4 1 0 50 -1 0 9 0.0000 2 135 540 %d %d %s\\001\n"
+    x (y-80) label
 
   where
   halfHeaderWidth = 200
@@ -42,7 +44,7 @@ timelineSpacing :: Int
 timelineSpacing = 500
 
 timelineLen :: Int
-timelineLen = 6500
+timelineLen = 5300
 
 timelineStart n = (timelineLen + timelineSpacing) * n
 
@@ -53,16 +55,19 @@ procTime :: Int
 procTime = 50
 
 propSpacing :: Int
-propSpacing = 290
+propSpacing = 230
 
-tellPrepare :: Int -> String -> String -> Writer String Int
-tellPrepare yStart ballot _ = tellRPC x1 yStart 620 $ \labelY -> tell $ printf
-  "4 2 0 50 -1 0 12 0.0000 2 180 1890 935 %d $\\\\mathbf{prepare}(%s)$\\001\n"
-  (labelY + labelOffset) ballot
+tellPrepare :: Int -> String -> Writer String Int
+tellPrepare = tellPrepare' 0
 
-tellPropose :: Int -> String -> String -> String -> Writer String Int
-tellPropose yStart inst ballot _ = tellRPC x2 yStart 350 $ \labelY -> tell $ printf
-  "4 0 0 50 -1 0 12 0.0000 2 180 2160 2160 %d $\\\\mathbf{proposed}_{%s}(%s)$\\001\n"
+tellPrepare' :: Int -> Int -> String -> Writer String Int
+tellPrepare' extraLabelOffset yStart ballot = tellRPC x1 yStart 570 $ \labelY -> tell $ printf
+  "4 2 0 50 -1 0 9 0.0000 2 180 1890 950 %d $\\\\mathbf{prepare}(%s)$\\001\n"
+  (labelY + labelOffset + extraLabelOffset) ballot
+
+tellPropose :: Int -> String -> String -> Writer String Int
+tellPropose yStart inst ballot = tellRPC x2 yStart 350 $ \labelY -> tell $ printf
+  "4 0 0 50 -1 0 9 0.0000 2 180 2160 2050 %d $\\\\mathbf{proposed}_{%s}(%s)$\\001\n"
   (labelY + labelOffset) inst ballot
 
 tellNewEra :: Int -> Writer String ()
@@ -70,60 +75,111 @@ tellNewEra y = do
   tell $ printf "2 1 1 1 0 7 50 -1 -1 4.000 0 0 -1 0 0 2\n"
   tellCoords (x1 - 500) y' (x2 + 500) y'
 
-  tell $ printf "4 2 0 50 -1 0 12 0.0000 2 180 1890 935 %d era $e$\\001\n" (y' - 80)
-  tell $ printf "4 2 0 50 -1 0 12 0.0000 2 180 1890 935 %d era $e+1$\\001\n" (y' + 200)
+  tell $ printf "4 2 0 50 -1 0 9 0.0000 2 180 1890 935 %d era $e$\\001\n" (y' - 80)
+  tell $ printf "4 2 0 50 -1 0 9 0.0000 2 180 1890 935 %d era $e+1$\\001\n" (y' + 160)
 
   where y' = y - div procTime 2
 
 main :: IO ()
-main = writeFile "test.fig" $ execWriter $ do
-  tell $ unlines
-    [ "#FIG 3.2  Produced by xfig version 3.2.5c"
-    , "Landscape"
-    , "Center"
-    , "Metric"
-    , "A4      "
-    , "100.00"
-    , "Single"
-    , "-2"
-    , "1200 2"
-    ]
+main = forM_
+    [ ("stoppable.fig",  tellStoppable)
+    , ("stoppable2.fig", tellStoppable2)
+    , ("smooth.fig",     tellSmooth)
+    , ("dynamic.fig",    tellDynamic)
+    ] $ \(fileName, tellSequenceDiagram) -> do
+    
+  writeFile fileName $ execWriter $ do
+    tell $ unlines
+      [ "#FIG 3.2  Produced by xfig version 3.2.5c"
+      , "Landscape"
+      , "Center"
+      , "Metric"
+      , "A4      "
+      , "100.00"
+      , "Single"
+      , "-2"
+      , "1200 2"
+      ]
 
-  let timelines = [(xLeader, "$\\\\ell$"), (x1, "I"), (x2, "II")]
-  forM_ [0..1] $ forM_ timelines . uncurry . tellTimeline
-
-  tellStoppable  (100 + timelineStart 0)
-  tellSmooth     (100 + timelineStart 1)
+    let timelines = [(xLeader, "$\\\\ell$"), (x1, "I"), (x2, "II")]
+    forM_ timelines $ uncurry $ tellTimeline 0
+    tellSequenceDiagram 0
+  
+  callCommand ("fig2ps  --nogv " ++ fileName)
+  callCommand ("fig2pdf --nogv " ++ fileName)
 
 tellSmooth firstPrepareStartTime = do
-  firstPrepareFinishTime <- tellPrepare firstPrepareStartTime "b" "e"
+  firstPrepareFinishTime <- tellPrepare firstPrepareStartTime "b"
 
   finishTimes <- forM [0..3] $ \n -> tellPropose (firstPrepareFinishTime + propSpacing * n)
-    (if n == 0 then "i" else printf "i+%d" n) "b" "e"
+    (showIPlus n) "b"
 
   let secondPrepareStartTime = last finishTimes
-  secondPrepareFinishTime <- tellPrepare secondPrepareStartTime "b'" "e+1"
+  secondPrepareFinishTime <- tellPrepare secondPrepareStartTime "b'"
 
   tellNewEra $ last finishTimes
 
-  forM_ [4..12] $ \n -> let
+  forM_ [4..13] $ \n -> let
     proposeStartTime = firstPrepareFinishTime + propSpacing * n
     in tellPropose proposeStartTime
           (printf "i+%d" n)
           (if proposeStartTime >= secondPrepareFinishTime then "b'" else "b")
-          "e+1"
 
 tellStoppable firstPrepareStartTime = do
-  firstPrepareFinishTime <- tellPrepare firstPrepareStartTime  "b" "e"
+  firstPrepareFinishTime <- tellPrepare firstPrepareStartTime  "b"
 
   finishTimes <- forM [0..3] $ \n -> tellPropose (firstPrepareFinishTime + propSpacing * n)
-    (if n == 0 then "i" else printf "i+%d" n) "b" "e"
+    (showIPlus n) "b"
 
   tellNewEra $ last finishTimes
 
   let secondPrepareStartTime = last finishTimes
-  secondPrepareFinishTime <- tellPrepare secondPrepareStartTime "b'" "e+1"
+  secondPrepareFinishTime <- tellPrepare secondPrepareStartTime "b'"
 
-  forM_ [4..7] $ \n -> tellPropose (secondPrepareFinishTime + propSpacing * (n - 4))
-    (printf "i+%d" n) "b'" "e + 1"
+  forM_ [4..6] $ \n -> tellPropose (secondPrepareFinishTime + propSpacing * (n - 4))
+    (showIPlus n) "b'"
 
+showIPlus :: Int -> String
+showIPlus 0 = "i"
+showIPlus n = printf "i+%d" n
+    
+tellStoppable2 firstPrepareStartTime = do
+  firstPrepareFinishTime <- tellPrepare firstPrepareStartTime "b"
+  
+  let startIndex n
+          | n == 1    = 2
+          | n == 2    = 3
+          | n == 3    = 1
+          | otherwise = n
+
+  finishTimes <- forM [0..3] $ \n -> tellPropose
+    (firstPrepareFinishTime + propSpacing * startIndex n)
+    (showIPlus n) "b"
+
+  tellNewEra $ maximum finishTimes
+
+  let secondPrepareStartTime = last finishTimes
+  secondPrepareFinishTime <- tellPrepare' 220 secondPrepareStartTime "b'"
+
+  forM_ [4..6] $ \n -> tellPropose (secondPrepareFinishTime + propSpacing * (n - 4))
+    (showIPlus n) "b'"
+
+tellDynamic firstPrepareStartTime = do
+  firstPrepareFinishTime <- tellPrepare firstPrepareStartTime "b"
+
+  finishTimeses <- forM [0,1] $ \n0 -> let
+    go n y
+      | n < 6 = do
+          finishTime <- tellPropose y (showIPlus n) "b"
+          (:) finishTime <$> go (n+2) finishTime
+      | otherwise = return []
+    in go n0 (firstPrepareFinishTime + propSpacing * n0)
+
+  let finishTimes = sort $ concat finishTimeses 
+  
+  secondPrepareFinishTime <- tellPrepare' (-200) (finishTimes !! 3) "b'"
+  tellNewEra $ finishTimes !! 5
+  tellPropose  secondPrepareFinishTime                (showIPlus 6) "b'"
+  tellPropose (secondPrepareFinishTime + propSpacing) (showIPlus 7) "b'"
+  return ()
+  
